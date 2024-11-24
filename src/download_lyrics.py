0fb0d9339ba551debe581912b418f1c5fd12def7
Bruno Freudensprung <bruno.freudensprung@kairntech.com>
2024-11-24 12:06:40 +0100

    better management of html to text

--- src/download_lyrics.py
+++ src/download_lyrics.py
@@ -1,29 +1,46 @@
 import os
+import re
 
 import requests
 from bs4 import BeautifulSoup
 
 def download_lyrics(genius_url, output_dir, force=False):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    lyrics_html = f'{output_dir}/lyrics.html'
+    # if not os.path.exists(lyrics_html) or force:
+    #     r = requests.get(genius_url)
+    #     lyrics = r.text
+    #     with open(lyrics_html, mode="w", encoding="utf-8") as file:
+    #         file.write(lyrics)
     lyrics_txt = f'{output_dir}/lyrics.txt'
     if os.path.exists(lyrics_txt) and not force:
         return lyrics_txt
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    r = requests.get(genius_url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    with open(lyrics_txt, mode="w", encoding="utf-8") as file:
-        lyrics_divs = soup.find_all(lambda tag : tag.has_attr('data-lyrics-container'))
-        for lyrics_div in lyrics_divs:
-            text_nodes = lyrics_div.find_all(string=True)
-            for text_node in text_nodes:
-                line = str(text_node)
-                if not "[" in line:
-                    file.write(line)
-                    file.write('\n')
+    with open(lyrics_html, mode="r", encoding="utf-8") as file:
+        lyrics = file.read()
+        lyrics = lyrics.replace('\r', '').replace('\n', '')
+        lyrics = re.sub('<span[^>]*>', '', lyrics)
+        lyrics = lyrics.replace('</span>', '')
+        # lyrics = re.sub('<div[^>]*>', '', lyrics)
+        # lyrics = lyrics.replace('</div>', '')
+        lyrics = re.sub('<a[^>]*>', '', lyrics)
+        lyrics = lyrics.replace('</a>', '')
+        with open(f'{output_dir}/lyrics-clean.html', mode="w", encoding="utf-8") as html:
+            html.write(lyrics)
+        soup = BeautifulSoup(lyrics, "html.parser")
+        with open(lyrics_txt, mode="w", encoding="utf-8") as file:
+            lyrics_divs = soup.find_all(lambda tag : tag.has_attr('data-lyrics-container'))
+            for lyrics_div in lyrics_divs:
+                text_nodes = lyrics_div.find_all(string=True)
+                for text_node in text_nodes:
+                    line = str(text_node)
+                    if not "[" in line:
+                        file.write(line)
+                        file.write('\n')
 
 if __name__ == '__main__':
     from sample_projects import get_sample_project_items
-    project = 'dancing_in_the_dark'
-    # project = 'ma_direction'
+    # project = 'dancing_in_the_dark'
+    project = 'ma_direction'
     project_dir, genius_url = get_sample_project_items(project, 'project_dir', 'genius_url')
     download_lyrics(genius_url, project_dir, force=True)
