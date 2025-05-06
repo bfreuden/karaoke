c954f1cc9296e538691f08545145909004a06b1b
Bruno Freudensprung <bruno.freudensprung@kairntech.com>
2025-05-07 00:03:01 +0200

    add optional suffix

--- src/replace_audio_track_in_video.py
+++ src/replace_audio_track_in_video.py
@@ -2,9 +2,9 @@ import os.path
 from pathlib import Path
 
 
-def replace_audio_track_in_video(video_mp4, accompaniment_mp3, force=False):
+def replace_audio_track_in_video(video_mp4, accompaniment_mp3, suffix="-accompaniment", force=False):
     project_dir = os.path.abspath(os.path.dirname(video_mp4))
-    video_accompaniment_mp4 = os.path.abspath(f"{project_dir}/{Path(os.path.basename(video_mp4)).stem}-accompaniment.mp4")
+    video_accompaniment_mp4 = os.path.abspath(f"{project_dir}/{Path(os.path.basename(video_mp4)).stem}{suffix}.mp4")
     if os.path.exists(video_accompaniment_mp4):
         if force:
             os.remove(video_accompaniment_mp4)
@@ -15,6 +15,7 @@ def replace_audio_track_in_video(video_mp4, accompaniment_mp3, force=False):
 
 if __name__ == '__main__':
     from projects import get_project_dir, get_project_data
-    project_name = 'slash-far-and-away'
+    project_name = 'hudson-hawk-swinging-on-a-star'
+    project_name = 'afi-medicate'
     project_dir = get_project_dir(project_name)
-    replace_audio_track_in_video(f'{project_dir}/video.mp4', f'{project_dir}/accompaniment.mp3', force=True)
+    replace_audio_track_in_video(f'{project_dir}/video-accompaniment.mp4', f'{project_dir}/accompaniment-with-choirs.mp3', "-with-choirs", force=True)
